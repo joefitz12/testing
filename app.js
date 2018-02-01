4cfbc0b5b38216ef3a382718732522cfa44db158
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-01 11:46:28 -0600

    adds / to redirect URL

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("working");
 $(document).on("click", function() {
-    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&redirect_uri=https://joefitz12.github.io/testing");
+    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&redirect_uri=https://joefitz12.github.io/testing/");
 })
 
 console.log("Url is", window.location.href);
