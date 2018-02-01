414a56d87519601bd03efcf2155151fce8dd63b8
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-01 12:59:00 -0600

    adds ajax call

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+var tokenURL = "";
+
 console.log("working");
 $(document).on("click", function() {
     window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&redirect_uri=https://joefitz12.github.io/testing/");
 })
 
-console.log("Url is", window.location.href);
-var token = "";
+tokenURL = window.location.href;
+
+
+var URLArray = tokenURL.split("");
+var first = URLArray.indexOf("=") + 1;
+var last = URLArray.indexOf("&");
+var token = tokenURL.substring(first, last);
+
+if (!first){
+    $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+        'Authorization': 'Bearer ' + token
+        },
+        success: function(response) {
+        console.log(response);
+        }
+    });
+}
+
+
