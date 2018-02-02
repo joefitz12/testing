0da904928745dc0b051f6f6539ba109bf351bf03
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-02 11:11:39 -0600

    updates onclick

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ if (last > 0){
     });
 }
 
-$("#followArtist").on("click", function(){
+$("#followButton").on("click", function(){
     window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-modify&redirect_uri=https://joefitz12.github.io/testing/");
 
     var URLArray = tokenURL.split("");
