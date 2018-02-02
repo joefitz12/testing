bc6ee53db6316b611174cb08e1341770cc46d193
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-02 11:42:58 -0600

    makes tokenURL function global

--- app.js
+++ app.js
@@ -60,6 +60,8 @@ $("#followButton").on("click", function(){
     window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-modify&redirect_uri=https://joefitz12.github.io/testing/");
 });
 
+tokenURL = window.location.href;
+
 var URLArray = tokenURL.split("");
 var first = URLArray.indexOf("=") + 1;
 var last = URLArray.indexOf("&");
