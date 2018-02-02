39b61fd04536c37e4020a236b9fec94ea24d7e33
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-02 11:32:55 -0600

    reorganizes variable creation

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var tokenURL = "";
 $("#login").on("click", function(){
     window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-read&redirect_uri=https://joefitz12.github.io/testing/");
 
+    tokenURL = window.location.href;
+
+    var URLArray = tokenURL.split("");
+    var first = URLArray.indexOf("=") + 1;
+    var last = URLArray.indexOf("&");
+    var token = tokenURL.substring(first, last);
+
     if (last > 0){
         $.ajax({
             url: 'https://api.spotify.com/v1/me/following?type=artist&limit=50',
@@ -47,14 +54,6 @@ $("#login").on("click", function(){
         });
     }
 });
-
-tokenURL = window.location.href;
-
-var URLArray = tokenURL.split("");
-var first = URLArray.indexOf("=") + 1;
-var last = URLArray.indexOf("&");
-var token = tokenURL.substring(first, last);
-
 
 
 $("#followButton").on("click", function(){
