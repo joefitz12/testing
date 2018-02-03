490d84a9af71daf5fdb6055e669207e1e5e4de61
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-03 15:24:42 -0600

    fixes redirect

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var artistArr = [];
 var tokenURL = "";
 
 $("#login").on("click", function(){
-    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-read&redirect_uri=https://joefitz12.github.io/testing/");f
+    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-read&redirect_uri=https://joefitz12.github.io/testing/");
 });
     
 tokenURL = window.location.href;
