496dbe5769150ffe83b51e7e7d70cc29596403b3
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-03 15:19:04 -0600

    reorders ajax calls

--- app.js
+++ app.js
@@ -14,7 +14,47 @@ var last = URLArray.indexOf("&");
 var token = tokenURL.substring(first, last);
 
 if (last > 0){
+    $.ajax({
+        url: 'https://api.spotify.com/v1/me/following?type=artist&limit=50',
+        headers: {
+        'Authorization': 'Bearer ' + token
+        },
+
+        success: function(response) {
+
+            var followList = function (){
+                for (i = 0; i < response.artists.items.length; i++){
+                followArray.push(response.artists.items[i].name);
+                }
+            }
+
+            var artistRender = function(){
+                for (i = 0; i < followArray.length; i++){
+                    var newDiv = $("<p>");
+                    newDiv.addClass("artistDiv");
+                    newDiv.attr("data-artist",followArray[i]);
+                    newDiv.text(followArray[i]);
+                    newDiv.on("click", function(){
+                        if (artistArr.indexOf($(this).attr("data-artist")) < 0){
+                            artistArr.push($(this).attr("data-artist"));
+                            $(this).addClass("selectedArtist");
+                        }
+                        else {
+                            artistArr.splice(artistArr.indexOf($(this).attr("data-artist")), 1);
+                            $(this).removeClass("selectedArtist");
+                        }
+                    });
+                    $("#artistList").append(newDiv);
+                }
+            }
+
+            followList();
+        }
+    });
+    
     if (localStorage.getItem("follow")){
+
+        followArray.push(artist);
 
         console.log("local storage added?", localStorage.getItem("follow"));
 
@@ -57,49 +97,13 @@ if (last > 0){
 
     };
 
-    $.ajax({
-        url: 'https://api.spotify.com/v1/me/following?type=artist&limit=50',
-        headers: {
-        'Authorization': 'Bearer ' + token
-        },
-
-        success: function(response) {
-
-            var followList = function (){
-                for (i = 0; i < response.artists.items.length; i++){
-                followArray.push(response.artists.items[i].name);
-                }
-            }
-
-            var artistRender = function(){
-                for (i = 0; i < followArray.length; i++){
-                    var newDiv = $("<p>");
-                    newDiv.addClass("artistDiv");
-                    newDiv.attr("data-artist",followArray[i]);
-                    newDiv.text(followArray[i]);
-                    newDiv.on("click", function(){
-                        if (artistArr.indexOf($(this).attr("data-artist")) < 0){
-                            artistArr.push($(this).attr("data-artist"));
-                            $(this).addClass("selectedArtist");
-                        }
-                        else {
-                            artistArr.splice(artistArr.indexOf($(this).attr("data-artist")), 1);
-                            $(this).removeClass("selectedArtist");
-                        }
-                    });
-                    $("#artistList").append(newDiv);
-                }
-            }
-
-            followList();
-            artistRender();
-        }
-    });
+    artistRender();
+    
 };
 
 
 $("#followButton").on("click", function(){
     var artist = String($("#followArtist").val());
     localStorage.setItem("follow", artist);
-    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-modify&redirect_uri=https://joefitz12.github.io/testing/g");
+    window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&scope=user-follow-modify&redirect_uri=https://joefitz12.github.io/testing/");
 });
