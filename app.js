a2c0020784addcb4161265962ba23719013d83db
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-03 15:06:38 -0600

    reorders ajax calls

--- app.js
+++ app.js
@@ -14,6 +14,49 @@ var last = URLArray.indexOf("&");
 var token = tokenURL.substring(first, last);
 
 if (last > 0){
+    if (localStorage.getItem("follow")){
+
+        console.log("local storage added?", localStorage.getItem("follow"));
+
+        tokenURL = window.location.href;
+
+        var URLArray = tokenURL.split("");
+        var first = URLArray.indexOf("=") + 1;
+        var last = URLArray.indexOf("&");
+        var token = tokenURL.substring(first, last);
+        var spotifyID = "";
+        var artist = localStorage.getItem("follow");
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/search?q=' + artist + '&type=artist',
+            headers: {
+            'Authorization': 'Bearer ' + token
+            },
+
+            success: function(response) {
+                spotifyID = response.artists.items[0].id;
+                console.log(spotifyID);
+
+                $.ajax({
+                    method: "PUT",
+                    url: 'https://api.spotify.com/v1/me/following?type=artist&ids=' + spotifyID,
+                    headers: {
+                    'Authorization': 'Bearer ' + token
+                    },
+            
+                    success: function(response) {
+                        console.log(artist, "followed");
+                    }
+                });
+            }
+        });
+
+        localStorage.removeItem("follow");
+
+        console.log("local storage cleared?", localStorage.getItem("follow"));
+
+    };
+
     $.ajax({
         url: 'https://api.spotify.com/v1/me/following?type=artist&limit=50',
         headers: {
@@ -30,7 +73,6 @@ if (last > 0){
 
             var artistRender = function(){
                 for (i = 0; i < followArray.length; i++){
-                    console.log("artist", followArray[i]);
                     var newDiv = $("<p>");
                     newDiv.addClass("artistDiv");
                     newDiv.attr("data-artist",followArray[i]);
@@ -53,47 +95,6 @@ if (last > 0){
             artistRender();
         }
     });
-    if (localStorage.getItem("follow")){
-
-        console.log("local storage?", localStorage.getItem("follow"));
-
-        tokenURL = window.location.href;
-
-        var URLArray = tokenURL.split("");
-        var first = URLArray.indexOf("=") + 1;
-        var last = URLArray.indexOf("&");
-        var token = tokenURL.substring(first, last);
-        var spotifyID = "";
-        var artist = localStorage.getItem("follow");
-        console.log("artist", artist);
-
-        $.ajax({
-            url: 'https://api.spotify.com/v1/search?q=' + artist + '&type=artist',
-            headers: {
-            'Authorization': 'Bearer ' + token
-            },
-
-            success: function(response) {
-                spotifyID = response.artists.items[0].id;
-                console.log(spotifyID);
-
-                $.ajax({
-                    method: "PUT",
-                    url: 'https://api.spotify.com/v1/me/following?type=artist&ids=' + spotifyID,
-                    headers: {
-                    'Authorization': 'Bearer ' + token
-                    },
-            
-                    success: function(response) {
-                        console.log(response);
-                        console.log(artist, "followed");
-                    }
-                });
-            }
-        });
-
-        localStorage.removeItem("follow");
-    };
 };
 
 
